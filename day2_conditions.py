5fe797ae34b06f0570ac0b8421721abcb24e933c
Shakira fatima <skaterasad88@gmail.com>
2026-01-28 16:44:19 +0530

    Completed day 2-if else conditions practice

---
+++ day2_conditions.py
@@ -0,0 +1,10 @@
+print("Day 2 Practice")
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(num, "is an Even number")
+else:
+    print(num, "is an Odd number")
+
+print("Day 2 completed successfully!")
