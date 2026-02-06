cc2ad3827ba265b0f06b518469bb864fe426f396
Shakira fatima <skaterasad88@gmail.com>
2026-02-06 22:32:39 +0530

    Completed day7_dictionaries

---
+++ Day7_dictionaries.py
@@ -0,0 +1,32 @@
+#program1:student marks
+marks = {
+    "Maths": 85,
+    "Science": 90,
+    "English": 78
+}
+
+total = sum(marks.values())
+print("Total marks:", total)
+
+#program2:creating dictionary
+student = {
+    "name": "Shakira",
+    "age": 18,
+    "course": "Python", 
+    " hobby":"coding", 
+}
+print("student") 
+
+#program3:changing dictionary
+student["age"]=20
+print(student) 
+student["hobby"]="reading"
+print(student)
+
+#program4:removing dictionary
+del student["course"]
+print(student)
+         
+#program5:adding dictionary
+student["city"] = "es())"
+print(student)
